6d64f41772bad38d48bdad714a6b75bc3129bf4b
MGxCoder <manas22gaikwad@gmail.com>
2025-10-04 22:47:57 +0530

    Initial commit

---
+++ frontend/src/landing_page/about/AboutPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Hero from "./Hero";
+import Team from "./Team";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function PricingPage() {
+  return (
+    <>
+      <Hero />
+      <Team />
+    </>
+  );
+}
+
+export default PricingPage;
